b0c031103879a54c884ac4d9f7c5bc1b07710ffb
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-04-16 07:08:33 +0100

    ** Add Types to Models using typegoose

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,26 +1,58 @@
-import mongoose, { Schema } from "mongoose";
+import { ObjectType, Field, ID, Int } from "type-graphql";
+import { prop as Property, getModelForClass } from "@typegoose/typegoose";
 
-const postSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true
-    },
-    imageUrl: {
-      type: String,
-      required: true
-    },
-    content: {
-      type: String,
-      required: true
-    },
-    creator: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true
-    }
-  },
-  { timestamps: true }
-);
+import { Ref } from "../types";
+import { User } from "./user";
 
-export default mongoose.model('Post', postSchema);
+@ObjectType({description: 'The Post model'})
+export class Post {
+  @Field(() => ID)  
+  id!: String;
+
+  @Field()
+  @Property({required: true})
+  title!: String;
+
+  @Field()
+  @Property({required: true})
+  imageUrl!: string;
+
+  @Field()
+  @Property({required: true})
+  content!: String;
+
+  @Field((type) => String)
+    @Property({ref: User, required: true})
+    creator!: User;
+
+    _doc:any
+    createdAt: any
+    updatedAt: any
+}
+
+export const PostModel = getModelForClass(Post, {schemaOptions: {timestamps: true}});
+
+// const postSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true
+//     },
+//     imageUrl: {
+//       type: String,
+//       required: true
+//     },
+//     content: {
+//       type: String,
+//       required: true
+//     },
+//     creator: {
+//       type: Schema.Types.ObjectId,
+//       ref: 'User',
+//       required: true
+//     }
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model('Post', postSchema);
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,29 +1,62 @@
+import { ObjectType, Field, ID, Int } from "type-graphql";
+import { prop as Property, getModelForClass, mongoose } from "@typegoose/typegoose";
 
-import mongoose, { Schema } from "mongoose";
+import {Post} from "./post";
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: 'I am new!'
-  },
-  posts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Post'
-    }
-  ]
-});
 
-export default mongoose.model('User', userSchema);
+@ObjectType({description: 'The User model'})
+export class User {
+  @Field(() => ID)  
+  id!: String;
+
+  @Field()
+  @Property({required: true})
+  email!: string;
+
+  @Field()
+  @Property({required: true})
+  password!: string;
+
+  @Field()
+  @Property({required: true})
+  name!: String;
+
+  @Field()
+  @Property({default: 'I am new!'})
+  status!: String;
+
+  @Field((type) => [Post])
+    @Property({type: () => Post, default: []})
+    posts!: mongoose.Types.Array<Post>;
+    _doc:any
+
+}
+
+export const UserModel = getModelForClass(User);
+
+// const userSchema = new Schema({
+//   email: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   },
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   status: {
+//     type: String,
+//     default: 'I am new!'
+//   },
+//   posts: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: 'Post'
+//     }
+//   ]
+// });
+
+// export default mongoose.model('User', userSchema);
